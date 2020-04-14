62b00aee070f5a26cd669a46a5dc7c8f83ae872d
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-04-15 08:55:49 +0900

    heh

--- setup.py
+++ setup.py
@@ -2,23 +2,22 @@ import os
 import re
 import pathlib
 from setuptools import setup, find_packages
-from miyadaiku.common import setuputils
+from miyadaiku import setuputils
 
 DIR = pathlib.Path(__file__).resolve().parent
-os.chdir(DIR)
 
 requires = [
     "miyadaiku"
 ]
 
-srcdir = 'node_modules/tether/dist/js'
-destdir = 'miyadaiku/themes/tether/externals/js'
+srcdir = DIR / 'node_modules/tether/dist/js'
+destdir = DIR / 'miyadaiku_theme_tether/externals/js'
 copy_files = [[srcdir, ['*.js'], destdir]]
-versionpy = DIR / 'miyadaiku/themes/tether/__version__.py'
+versionpy = DIR / 'miyadaiku_theme_tether/__version__.py'
 version = re.search(r'"([\d.]+)"', versionpy.read_text())[1]
 
 setup(
-    name="miyadaiku.themes.tether",
+    name="miyadaiku_theme_tether",
     version=version,
     author="Atsuo Ishimoto",
     license="MIT",
@@ -29,12 +28,12 @@ setup(
     ],
     description='Tether files for miyadaiku static site generator',
     long_description=setuputils.read_file(DIR, 'README.rst'),
-    url='https://github.com/miyadaiku/miyadaiku-themes-tether',
+    url='https://github.com/miyadaiku/miyadaiku_theme_tether',
     project_urls={
         'Miyadaiku': 'https://miyadaiku.github.io/',
     },
 
-    packages=list(setuputils.list_packages(DIR, 'miyadaiku')),
+    packages=list(setuputils.list_packages(DIR, 'miyadaiku_theme_tether')),
     package_data={
         '': setuputils.SETUP_FILE_EXTS,
     },
